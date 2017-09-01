9f0c3a965957a9260b988db8033221ceb2d5e9ab
davetromp <david.tromp@maakjehuishoom.nl>
2017-09-01 09:41:13 +0200

    add filtering of best configfiles and copy these to the active folder

--- backtest.py
+++ backtest.py
@@ -1,27 +1,61 @@
-import os
+import os, sys
+import pandas as pd
+from shutil import copyfile
 
-print "remove the backtests.csv file"
-os.remove("backtests.csv")
 
-print "remove all backtest result files from folder tests"
-for ff in os.listdir("tests/"):
-    os.remove("tests/{}".format(ff))
+def runTests():
+    print "remove the backtests.csv file"
+    os.remove("backtests.csv")
 
-print "remove all cached data files"
-for fff in os.listdir("cache/"):
-    os.remove("cache/{}".format(fff))
+    print "remove all backtest result files from folder tests"
+    for ff in os.listdir("tests/"):
+        os.remove("tests/{}".format(ff))
 
-print "running backtests aginst all config files in folder configs"
-# print "MARKET,TF,MEAN,BREAKOUT,RTM,RTM_PERCENT,BO_PERCENT,EXIT_PERCENT,sharpe_ratio"
-os.system("""echo "MARKET,TF,MEAN,BREAKOUT,RTM,RTM_PERCENT,BO_PERCENT,EXIT_PERCENT,sharpe_ratio" >> backtests.csv""")
-files = os.listdir("configs/")
-count = 1
-for f in files:
-    configfile = "configs/" + f
-    backtestfile = "tests/" + f[:-6] + ".png"
-    os.system("python main.py -c {} -b {} -m >> backtests.csv".format(
-        configfile,
-        backtestfile
-    ))
-    print count
-    count += 1
+    print "remove all cached data files"
+    for fff in os.listdir("cache/"):
+        os.remove("cache/{}".format(fff))
+
+    print "running backtests aginst all config files in folder configs"
+    # print "MARKET,TF,MEAN,BREAKOUT,RTM,RTM_PERCENT,BO_PERCENT,EXIT_PERCENT,sharpe_ratio"
+    os.system("""echo "MARKET,TF,MEAN,BREAKOUT,RTM,RTM_PERCENT,BO_PERCENT,EXIT_PERCENT,sharpe_ratio" >> backtests.csv""")
+    files = os.listdir("configs/")
+    count = 1
+    for f in files:
+        configfile = "configs/" + f
+        backtestfile = "tests/" + f[:-6] + "png"
+        os.system("python main.py -c {} -b {} -m >> backtests.csv".format(
+            configfile,
+            backtestfile
+        ))
+        print count
+        count += 1
+
+def bestOnes():
+    df = pd.read_csv("backtests.csv")
+    markets = df['MARKET'].unique()[:-1]
+    dfmm = []
+    dfm = {}
+    for market in markets:
+        dfm[market] = df[df['MARKET'] == market]
+        dfm[market] = dfm[market][dfm[market]['sharpe_ratio'] == max(dfm[market]['sharpe_ratio'])]
+        dfm[market] = dfm[market].reset_index()#.ix[0]
+        dfm[market] = dfm[market][dfm[market].index == 0]
+        dfmm.append(dfm[market])
+    return pd.concat(dfmm).reset_index()
+
+
+if __name__ == "__main__":
+    runTests()
+    best = bestOnes()
+    for i in best.index:
+        base = "_".join(list(best.ix[i])[2:-1])
+        configfile = base + ".config"
+        try:
+            copyfile("configs/" + configfile, "active/" +configfile)
+        except Exception as e:
+            print str(e)
+        plotfile = base + "..png"
+        try:
+            copyfile("tests/" + plotfile, "best/" +plotfile)
+        except Exception as e:
+            print str(e)
