88b2756566403c46d0d4098237aa7bf3f3ff5af5
davetromp <david.tromp@maakjehuishoom.nl>
2017-09-02 23:44:00 +0200

    simplify the generated profiles

--- generate.py
+++ generate.py
@@ -7,63 +7,51 @@ for f in os.listdir("configs/"):
 print "generating new configfiles in folder configs"
 trades = ['BTC']
 for trade in trades:
-    # currencies = ['LTC', 'ETH', 'BCC', 'XRP', 'XMR', 'DASH', 'NEO', 'OMG', 'STRAT', 'ETC']
-    currencies = ['APX', 'RISE', 'MCO', 'LSK', 'XVG', 'NAV', 'TRIG', 'PAY', 'QTUM', 'MTL', 'BAT', 'ADX']
+    currencies = ['LTC', 'ETH', 'BCC', 'XRP', 'XMR', 'DASH', 'NEO', 'OMG', 'STRAT', 'ETC',
+                  'APX', 'RISE', 'MCO', 'LSK', 'XVG', 'NAV', 'TRIG', 'PAY', 'QTUM', 'MTL', 'BAT', 'ADX']
     for currency in currencies:
-        timeframes = [1, 5]
+        timeframes = [5]
         for tf in timeframes:
             means = [8, 20]
             for mean in means:
-                breakout = [True]
-                for bo in breakout:
-                    meanreversion = [True]
-                    if bo == False:
-                        meanreversion = [True]
-                    for mr in meanreversion:
-                        if mr == True:
-                            rtm_percs = [2.0, 3.0, 5.0]
-                        else:
-                            rtm_percs = [2.0]
-                        for rtm_perc in rtm_percs:
-                            if bo == True:
-                                bo_percs = [1.0, 2.0, 3.0]
-                            else:
-                                bo_percs = [1.0]
-                            for bo_perc in bo_percs:
-                                exit_percs = [1.0, 2.0]
-                                for exit_perc in exit_percs:
-                                    config = """
-                                    TRADE = {}
-                                    CURRENCY = {}
-                                    TF = {}
-                                    MEAN = {}
-                                    BREAKOUT = {}
-                                    RTM = {}
-                                    RTM_PERCENT = {}
-                                    BO_PERCENT = {}
-                                    EXIT_PERCENT = {}
-                                    QUANTITY = 1
-                                    LOWER_SIGNAL_BOUND = 1.0
-                                    UPPER_SIGNAL_BOUND = 1.0
-                                    UPPER_BUY_BOUND = 1.0
-                                    LOWER_BUY_BOUND = 5.0
-                                    FEE = 0.25
-                                    """.format(trade,
-                                               currency,
-                                               tf,
-                                               mean,
-                                               bo,
-                                               mr,
-                                               rtm_perc,
-                                               bo_perc,
-                                               exit_perc)
-                                    with open("configs/{}-{}_{}_{}_{}_{}_{}_{}_{}.config".format(trade,
-                                               currency,
-                                               tf,
-                                               mean,
-                                               bo,
-                                               mr,
-                                               rtm_perc,
-                                               bo_perc,
-                                               exit_perc), 'w') as configfile:
-                                        configfile.write(config)
+                rtm_percs = [2.0, 3.0, 5.0]
+                for rtm_perc in rtm_percs:
+                    bo_percs = [1.0]
+                    for bo_perc in bo_percs:
+                        exit_percs = [1.0, 5.0, 10.0]
+                        for exit_perc in exit_percs:
+                            for stop_perc in [5.0, 10.0, 15.0]:
+                                config = """
+                                TRADE = {}
+                                CURRENCY = {}
+                                TF = {}
+                                MEAN = {}
+                                BREAKOUT = True
+                                RTM = True
+                                RTM_PERCENT = {}
+                                BO_PERCENT = {}
+                                EXIT_PERCENT = {}
+                                QUANTITY = 1
+                                LOWER_SIGNAL_BOUND = 1.0
+                                UPPER_SIGNAL_BOUND = 1.0
+                                UPPER_BUY_BOUND = 1.0
+                                LOWER_BUY_BOUND = 5.0
+                                FEE = 0.25
+                                STOP_PERC = {}
+                                """.format(trade,
+                                           currency,
+                                           tf,
+                                           mean,
+                                           rtm_perc,
+                                           bo_perc,
+                                           exit_perc,
+                                           stop_perc)
+                                with open("configs/{}-{}_{}_{}_True_True_{}_{}_{}_{}.config".format(trade,
+                                                                                             currency,
+                                                                                             tf,
+                                                                                             mean,
+                                                                                             rtm_perc,
+                                                                                             bo_perc,
+                                                                                             exit_perc,
+                                                                                             stop_perc), 'w') as configfile:
+                                    configfile.write(config)
