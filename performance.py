f5239f51fcba2f92d5e1b56559b13ec67e7fcfdb
davetromp <david.tromp@maakjehuishoom.nl>
2017-09-04 21:04:36 +0200

    more adjustments

--- performance.py
+++ performance.py
@@ -8,15 +8,18 @@ import matplotlib.pyplot as plt
 from secret import *
 
 
-def getPerformance(market):
+def getPerformance(market, plot = True):
     try:
         API = bittrex(KEY, SECRET)
         orderhistory = API.getorderhistory(market, 20)
         if orderhistory:
             df = pd.DataFrame(orderhistory)
             df = df.sort_values('Closed')
+            df = df[df['Closed'] >= '2017-09-04']
+            df = df.reset_index()
+            if df['OrderType'][0] == "LIMIT_SELL":
+                df = df [1:]
             diffs = []
-            sells = []
             buy = 0.0
             sell = 0.0
             diffs.append(0.0)
@@ -25,17 +28,18 @@ def getPerformance(market):
                     buy += df['PricePerUnit'][i] * df['Quantity'][i]
                 else:
                     sell += df['PricePerUnit'][i] * df['Quantity'][i]
-                    # sells.append(sell)
-                    diffs.append(((sell - buy) / buy) * 100.0)
-            print "Difference between cumulative sells and buys"
-            print diffs[-1], "%"
-            plt.plot(diffs)
-            # plt.plot(sells)
-            plt.axhline(y=0.0, color='r', linestyle='-')
-            plt.title("Difference between cumulative sells and buys over time")
-            plt.ylabel("Difference %")
-            plt.xlabel("SELL Limit orders")
-            plt.show()
+                    diffs.append( ( (sell - buy) / buy ) * 100.0)
+            if plot:
+                print "Difference between cumulative sells and buys"
+                print diffs[-1], "%"
+                plt.plot(diffs)
+                plt.axhline(y=0.0, color='r', linestyle='-')
+                plt.title("Difference between cumulative sells and buys over time")
+                plt.ylabel("Difference %")
+                plt.xlabel("SELL Limit orders")
+                plt.show()
+            else:
+                return diffs
         else:
             print "There is no order history yet."
     except Exception as e:
